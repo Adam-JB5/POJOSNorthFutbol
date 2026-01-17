f0d5c36040d865f936b53396b1302420b65cacdc
Adam-JB5 <adamjanah562005@gmail.com>
2026-01-17 16:31:04 +0100

    Annadido ExcepcionNF

---
+++ src/pojosnorthfutbol/ExcepcionNF.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pojosnorthfutbol;
+
+/**
+ *
+ * @author adamj
+ */
+public class ExcepcionNF extends Exception {
+
+    private String sentenciaSQL;
+    private int codigoErrorBD;
+    private String mensajeErrorBD;
+    private String mensajeErrorUsuario;
+
+    public ExcepcionNF() {
+
+    }
+
+    public ExcepcionNF(String sentenciaSQL, int codigoErrorBD, String mensajeErrorBD, String mensajeErrorUsuario) {
+        this.sentenciaSQL = sentenciaSQL;
+        this.codigoErrorBD = codigoErrorBD;
+        this.mensajeErrorBD = mensajeErrorBD;
+        this.mensajeErrorUsuario = mensajeErrorUsuario;
+    }
+
+    public String getSentenciaSQL() {
+        return sentenciaSQL;
+    }
+
+    public void setSentenciaSQL(String sentenciaSQL) {
+        this.sentenciaSQL = sentenciaSQL;
+    }
+
+    public int getCodigoErrorBD() {
+        return codigoErrorBD;
+    }
+
+    public void setCodigoErrorBD(int codigoErrorBD) {
+        this.codigoErrorBD = codigoErrorBD;
+    }
+
+    public String getMensajeErrorBD() {
+        return mensajeErrorBD;
+    }
+
+    public void setMensajeErrorBD(String mensajeErrorBD) {
+        this.mensajeErrorBD = mensajeErrorBD;
+    }
+
+    public String getMensajeErrorUsuario() {
+        return mensajeErrorUsuario;
+    }
+
+    public void setMensajeErrorUsuario(String mensajeErrorUsuario) {
+        this.mensajeErrorUsuario = mensajeErrorUsuario;
+    }
+
+    public String toString() {
+        return "ExcepcionNF{" +
+           "sentenciaSQL=" + sentenciaSQL +
+           ", codigoErrorBD=" + codigoErrorBD +
+           ", mensajeErrorBD=" + mensajeErrorBD +
+           ", mensajeErrorUsuario=" + mensajeErrorUsuario +
+           '}';
+    }
+}
